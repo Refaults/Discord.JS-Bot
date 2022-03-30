cabc346413dac5995b740add0e4989a8848c7444
Refaults <101022642+Refaults@users.noreply.github.com>
2022-03-30 23:41:08 +0200

    Create tadd.js

---
+++ tadd.js
@@ -0,0 +1,35 @@
+client.on("message", message => {
+  if (message.content.startsWith("!add")) {
+        var topic = message.channel.topic;
+        if (!topic) {
+            message.channel.send("You cannot use this command here!");
+            return
+        }
+
+        if (topic.startsWith("User ID:")) {
+            var idUtente = topic.slice(9);
+            if (message.author.id == idUtente || message.member.hasPermission("MANAGE_CHANNELS")) {
+                var utente = message.mentions.members.first();
+                if (!utente) {
+                    message.channel.send("Please enter a valid user!");
+                    return
+                }
+
+                var haIlPermesso = message.channel.permissionsFor(utente).has("VIEW_CHANNEL", true)
+
+                if (haIlPermesso) {
+                    message.channel.send("This user already has access to the ticket!")
+                    return
+                }
+
+                message.channel.updateOverwrite(utente, {
+                    VIEW_CHANNEL: true
+                })
+
+                message.channel.send(`${utente.toString()} è stato aggiunto al ticket`)
+            }
+        }
+        else {
+            message.channel.send("You cannot use this command here!")
+        }
+    }
