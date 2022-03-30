ae18048d9c8d769ffc6a50fe8147393b3167b457
Refaults <101022642+Refaults@users.noreply.github.com>
2022-03-30 23:45:02 +0200

    Create sendimages.js

---
+++ sendimages.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("PASTE YOUR TOKEN HERE"); // Paste your Token Bot here
+
+// Commands
+client.on("message", (message) =>{
+    if(message.content == '!image'){ // Your command
+        message.channel.send("I send your image:", {files: ["Image1.jpg"]}); // Send one image
+    }
+})
+
+client.on("message", (message) =>{
+    if(message.content == '!image'){ // Your command
+        message.channel.send("I send your image:", {files: ["Image1.jpg", "Image2.jpg"]}); // Send two or more images
+    }
+})
