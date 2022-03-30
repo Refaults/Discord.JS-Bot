4d541605fe08dca4975c09a0419ac2b20e314708
Refaults <101022642+Refaults@users.noreply.github.com>
2022-03-30 23:32:16 +0200

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("PASTE YOUR TOKEN HERE"); // Paste your Token Bot here
+
+client.on('ready', () => {
+    console.log('The Bot is ONLINE!') // When the Bot is online, print this message
+    client.user.setActivity('\nSET_ACTIVITY_HERE', { type: 'PLAYING' }) // Set PLAYING - LISTENING - WATCHING
+    .catch(console.error); // If the bot has a errors, print the errors in the console
+});
